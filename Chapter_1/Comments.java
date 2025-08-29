4d02ce2bbcd38565fc72b838308ea79c299530b6
karl <mkarlelson@gmail.com>
2025-08-29 21:40:35 +0800

    Signed-off-by: karl <mkarlelson@gmail.com>

--- Chapter_1/Comments.java
+++ Chapter_1/Comments.java
@@ -1,11 +1,13 @@
 package Chapter_1;
-// the single line
+
 public class Comments  
 {
     public static void main(String[] args)
     {
+        
+        System.out.println("The 3-differant types of comments in Java are:");
+        // the single line
         /* the mutli-line */
-        System.out.println("Hi, this is comment");
-         /** the documentation */
+        /** the documentation */
     }
 }
