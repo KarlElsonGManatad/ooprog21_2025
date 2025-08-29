bdc77cfb7635621a8dfe71a20200d44afa296286
Karl <mkarlelson@gmail.com>
2025-08-29 16:38:25 +0800

    delete hello world and add 2nd activity
    Signed-off-by: Karl <mkarlelson@gmail.com>

---
+++ Chapter_1/FavorateSong.java
@@ -0,0 +1,14 @@
+package Chapter_1;
+
+ public class FavorateSong 
+ {
+ public static void main(String [] args) 
+ {
+ 
+   System.out.println("Pero wag mag-alala");
+   System.out.println("Di na kita gagambalain pa");
+   System.out.println("Alam ko namang ngayon");
+   System.out.println("May kapiling ka nang iba");
+   
+ }
+ }
