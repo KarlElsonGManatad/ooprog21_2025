864207d66a3eb33d5a8893963cefda7d7b7d8f38
karl <mkarlelson@gmail.com>
2025-09-19 08:59:58 +0800

    Signed-off-by: karl <mkarlelson@gmail.com>

---
+++ Chapter_3/CompanyInfo.java
@@ -0,0 +1,16 @@
+public class CompanyInfo {
+    public static void main(String[] args) {
+        // Print company name
+        System.out.println("Smart Electronics");
+
+        // Call the method to display business hours
+        displayHours();
+    }
+
+    // Method that prints the business hours
+    public static void displayHours() {
+        System.out.println("Monday - Friday 8 am to 6 pm");
+        System.out.println("Saturday        8 am to noon");
+        System.out.println("Sunday          closed");
+    }
+}
